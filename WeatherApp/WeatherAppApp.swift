6d7f40bffd0febb5b3913bf5b5e3ff72fceda2e9
See-Its-Manish <seeitsmanish@gmail.com>
2023-06-24 20:09:46 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Manish Sharma on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
